fde7f78a0548109735d4e230fccda7ecbea20450
Swetha Srinivasan <swethavasan97@gmail.com>
2022-03-03 15:23:39 +0530

    remove redundancy and refactoring

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,6 @@
 class TodosController < ApplicationController
-
+ before_action :set_todo, only: [:edit, :update, :show, :destroy]
+  
   	def new
  		@todo= Todo.new
 
@@ -16,15 +17,15 @@ class TodosController < ApplicationController
  	end 
 
  	def show
-  		@todo = Todo.find(params[:id])
+  		
 	end
 
 	def edit
-  		@todo = Todo.find(params[:id])
+  		
 	end
 
 	def update
-		@todo = Todo.find(params[:id])
+		
     	if @todo.update(todo_params)
     		flash[:notice]="Todo was successfully updated"
     		redirect_to todo_path(@todo)
@@ -32,11 +33,23 @@ class TodosController < ApplicationController
     		render 'edit'
   		end    
  	end 
+
  	def index
   		@todos = Todo.all
 	end
 
+	def destroy
+  		
+  		@todo.destroy
+  		flash[:notice] = "Todo was deleted successfully"
+  		redirect_to todos_path
+	end
+
  	private
+
+ 	def set_todo
+      @todo = Todo.find(params[:id])
+    end
 
   	def todo_params
   		params.require(:todo).permit(:name, :description)
